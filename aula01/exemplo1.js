96541d51cb28cb9aa6868676c6809b42070d82ca
leonardomateus10 <leonardomateus131415@gmail.com>
2026-02-24 11:36:59 -0300

    24/02

---
+++ aula01/exemplo1.js
@@ -0,0 +1,15 @@
+document.write("<h2>entrada de dados</h2>");
+
+var numero1 = prompt("digite o numero 1");
+var numero2 = prompt("digite o numero 2");
+
+document.write("numero 1 = " + numero1 + "<br>");
+document.write("numero 2 =" + numero2 + "<br>");
+
+
+document.write("<h2>operações aritméticas</h2>");
+var resultado;
+
+resultado = numero1 + numero2;
+document.write("a soma do numeros é" + resultado + "<br");
+
---
+++ aula01/script.js
@@ -0,0 +1,18 @@
+var nome = "leonardo";
+var idade = 16;
+var altura = 1.72;
+
+console.log ( nome );
+console.log ( idade );
+console.log ( altura );
+
+document.write("<h1>ola,seja bem vindo ao javaSrcipt </h1>")
+
+document.write ( nome + "<br>");
+document.write (idade + "<br>");
+document.write ( altura + "<br>" );
+
+var dias = idade * 365;
+document.write("<p style='color :red' > voce ja viveu " + dias + "dias</p>")
+
+
