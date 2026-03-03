061ec8b1ebdb0106d1ed64eee85f78da98aaef17
leonardomateus10 <leonardomateus131415@gmail.com>
2026-03-03 11:35:34 -0300

    03/03

---
+++ aula01/exercicio1.js
@@ -0,0 +1,11 @@
+document.write("<h1>aula 01 - exercicios 1 <h1/>")
+
+var numero = prompt("digite um número qualquer:")
+
+var quadrado = number (numero) * number(numero);
+var cubo = number (numero) * number (numero)* number(numero);
+
+document.write("<p> 0 numero digitado foi" + "<p/>");
+
+document.write("<P style ='color : color:red>o quadrado do numero é"+ quadrado + "</p>");
+document.write("<p style ='color : blue > o cubo do numero é" + cubo + "</p");
---
+++ aula01/exercicio2.js
@@ -0,0 +1,7 @@
+document.write("<h1>exercicio 2<h1/>")
+ var fahrenheit = prompt("digite a temperatura em fahrenheit");
+ var celsius = Number(fahrenheit - 32) * 5/9;
+ document.write("<p> a temperatura digitada foi" + fahrenheit + "F°</p>");
+
+ document.write(" <p style = 'color : red'> a temperatura em celsius é"+ celsius + "C</p>");
+
